ae94f8bcdd25ea475c7258b2a35824499680a5da
adwitafernanda <adwitafernanda4@gmail.com>
2023-03-08 17:18:48 +0800

    first commit

---
+++ latihan2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){//begin 
+
+//numeric luas, p, l
+double Luas, P, L;
+//display, "masukan panjangnya : "
+cout << "Masukkan Panjangnya : ";
+//accep
+cin >> P;
+//display, "masukkan lebarnya : "
+cout << "Masukkan Lebarnya : ";
+// accept l
+cin >> L;
+//compute Luas = P*L
+Luas = P*L;
+//display, "luasnya : " + luas
+cout << "Luasnya : " << Luas;
+return 0;
+}
